74f08191ba97d41604cb2b5037933775e4b2e265
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-16 09:22:50 +0700

    Enable Automatic Migration

---
+++ FPTBook/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace FPTBook.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FPTBook.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed= true;
+            ContextKey = "FPTBook.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(FPTBook.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

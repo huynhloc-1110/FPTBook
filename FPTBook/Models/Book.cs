8f63bebeaa9fb4cea86bfdbac2ac3361a84f8699
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-16 07:55:03 +0700

    Add Book, Category, CategoryRequest models

---
+++ FPTBook/Models/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FPTBook.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [StringLength(128, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(128, MinimumLength = 3)]
+        [Required]
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string CoverUrl { get; set; }
+
+        [Range(1,10000)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        [Required]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue)]
+        [Display(Name = "Quantity Left")]
+        [Required]
+        public int StockedQuantity { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Created at")]
+        [Required]
+        public DateTime CreatedDateTime { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Updated at")]
+        [Required]
+        public DateTime UpdatedDateTime { get; set; }
+    }
+}

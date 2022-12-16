4486c28689856a6972b243b86146b3cb7e7bfa21
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-16 07:15:45 +0700

    Add FullName property to user model

--- FPTBook/Models/ApplicationUser.cs
+++ FPTBook/Models/ApplicationUser.cs
@@ -6,12 +6,18 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace FPTBook.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
